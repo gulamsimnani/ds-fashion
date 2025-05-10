85733d36e374639b3b13809fc7a7fe8acca04fbf
gsimnani <gulam.simnani786@gmail.com>
2025-05-10 19:19:10 +0530

    checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CartItem, CartService } from '../services/cart.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
 })
