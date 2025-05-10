8d29a10a0081866a2910d358849e059c1b3d9518
gsimnani <gulam.simnani786@gmail.com>
2025-05-10 20:32:00 +0530

    cart updated

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CartItem, CartService } from '../services/cart.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
-export class MenuComponent {
-
+export class MenuComponent implements OnInit {
+  cartCount: number = 0;
+  constructor(private cartService: CartService) {}
+  ngOnInit(): void {
+    this.cartService.cartItems$.subscribe((items: CartItem[]) => {
+      this.cartCount = items.reduce((acc, item) => acc + item.quantity, 0);
+    });
+  }
 }
